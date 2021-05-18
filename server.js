026637e5ae9af6ed1c6b76915c08b49e223f328e
FcoCrespo <Fco.Crespo@alu.uclm.es>
2021-05-18 23:30:00 +0200

    fixing1

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+app.disable("x-powered-by");
+app.use(express.static(__dirname + '/dist/webesidevopsmetrics'));
+app.get('/*', function(req,res) {
+res.sendFile(path.join(__dirname+
+'/dist/webesidevopsmetrics/index.html'));});
+app.listen(process.env.PORT || 8080);
